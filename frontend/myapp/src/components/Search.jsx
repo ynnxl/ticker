9b27c6e8d1a9a6c9fc1ce5a8a990c95fbb804c48
Lenny Rolle <lennyrolle21@gmail.com>
2025-04-02 20:08:11 -0300

    Search Bug Fix

--- frontend/myapp/src/components/Search.jsx
+++ frontend/myapp/src/components/Search.jsx
@@ -1,28 +1,28 @@
 import React, { useState } from "react";
 
 function Search() {
-    const [input, setInput] = useState("");
-    const [bestMatches, setBestMatches] = useState(mockSearchResult.results);
+    // const [input, setInput] = useState("");
+    // const [bestMatches, setBestMatches] = useState(mockSearchResult.results);
 
-    const clear = () => {
-        setInput("");
-        setBestMatches([]);
-    }
-    const updateBestMatches = () => {
-        setBestMatches(mockSearchResult.result);
-    }
+    // const clear = () => {
+    //     setInput("");
+    //     setBestMatches([]);
+    // }
+    // const updateBestMatches = () => {
+    //     setBestMatches(mockSearchResult.result);
+    // }
     return (
         <div className="flex item-center my-4 border-2 rounded md relative z-50 w-96 bg-white border-neutral-200">
             <input type="text" 
-            value={input} 
+            // value={input} 
             className="w-full px-4 py-2 focus:outline-none rounder-md" 
             placeholder="Search stock..." 
-            onChange={(event) => {setInput(event.target.value);}}
-            onKeyPress={(event) => {
-                if (event.key === "Enter") {
-                    updateBestMatches();
-                }
-            }}
+            // onChange={(event) => {setInput(event.target.value);}}
+            // onKeyPress={(event) => {
+                // if (event.key === "Enter") {
+                //     updateBestMatches();
+                // }
+            // }}
             />
         </div>
     )
